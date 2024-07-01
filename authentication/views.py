c5597a63a92ee1e5961236f359b4e7f1cea68e87
taltalasuka <phamdungbmo@gmail.com>
2024-07-01 15:55:13 +0700

    feat: add sign up page

--- authentication/views.py
+++ authentication/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def login(request):
     return render(request, 'authentication/login.html')
+def signup(request):
+    return render(request, 'authentication/signup.html')
